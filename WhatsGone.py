53ae7c9f1f44a79dcb0d310f2f2d5572ff17792d
Thomas Boehnlein <radicalpope@gmail.com>
2025-03-16 18:44:34 -0400

    CHANGED: added folder make method ADDED: exclude filter support ADDED: file name tag to use ADDED: backup directory for the file

--- WhatsGone.py
+++ WhatsGone.py
@@ -4,6 +4,7 @@ import psutil
 import time
 import csv
 import os
+import shutil
 
 """
 This script provides functionality to interact with the WizTree application for scanning directories on a specified drive,
@@ -19,14 +20,15 @@ extracting file information, and processing the results. It includes the followi
 The script is designed to work with administrative privileges and assumes WizTree is installed and accessible at the specified path.
 """ 
 
-def get_files_with_wiztree(wiztree_path, drive, directory, output_file):
+def get_files_with_wiztree(wiztree_path, drive, tag, include_filter, exclude_filter, output_file):
     """
     Executes the WizTree application to retrieve file information from a specified directory 
     on a given drive and exports the results to an output file.
     Args:
         wiztree_path (str): The file path to the WizTree executable.
         drive (str): The drive letter to scan (e.g., "C:").
-        directory (str): The directory to filter within the specified drive.
+        include_filter (str): The directory to filter include the specified drive.
+        exclude_filter (str): The directory to filter exclude the specified drive.
         output_file (str): The file path where the WizTree output will be saved.
     Raises:
         subprocess.CalledProcessError: If the WizTree command fails to execute properly.
@@ -40,16 +42,13 @@ def get_files_with_wiztree(wiztree_path, drive, directory, output_file):
 
     try:
 
-        print(f"STARTED: Scanning {drive}\\ using filter {directory}")
+        print(f"STARTED {tag}: Scanning {drive}\\ using include {include_filter} and exclude {exclude_filter}")
 
         # Ensure the folder path for the output file exists
-        output_folder = os.path.dirname(output_file_path)
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-            print(f"Created folder: {output_folder}")
+        ensure_folder_exists(output_file_path)
         
         # Command to call WizTree with the specified directory and output file
-        command = [wiztree_path, drive, f"/filter={directory}", f"/export=\"{output_file}\"", r"/admin=1", r"/exportfolders=0"]
+        command = [wiztree_path, drive, f"/filter={include_filter}", f"/filterexclude={exclude_filter}", f"/export=\"{output_file}\"", r"/admin=1", r"/exportfolders=0"]
         
         admin_command = ["powershell", "-Command", f"Start-Process -FilePath '{command[0]}' -ArgumentList '{' '.join(command[1:])}' -Verb RunAs"]
 
@@ -58,7 +57,7 @@ def get_files_with_wiztree(wiztree_path, drive, directory, output_file):
         
         # Wait for WizTree to finish
         wait_for_process_to_finish("WizTree64.exe")
-        #wait_for_process_to_finish("WizTree.exe")
+        #wait_for_process_to_finish("WizTree.exe")        
 
         # Print the output from WizTree
         # print("WizTree Output:", result.stdout)
@@ -114,6 +113,8 @@ def process_output_file(output_file_path):
             print(f"ERROR: {output_file_path} has no files. Current files will not be overwritten.")
             return False
 
+        print(f"RECORDED: {len(first_items)-1} files")
+
         # Write the extracted items back to the same file, overwriting the original content
         with open(output_file_path, 'w', newline='', encoding='utf-8') as file:
             for item in first_items:
@@ -123,6 +124,20 @@ def process_output_file(output_file_path):
 
         return True
 
+# Ensure the folder path for the output file exists
+def ensure_folder_exists(file_path):
+    """
+    Ensures that the specified folder exists. If it does not exist, creates it.
+    
+    Args:
+        file_path (str): The path of the file to check or create.
+    """
+    folder_path = os.path.dirname(file_path)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Created folder: {folder_path}")
+
 if __name__ == "__main__":
 
 
@@ -131,23 +146,67 @@ if __name__ == "__main__":
 
     wiztree_executable = "C:/Program Files/WizTree/WizTree64.exe"
 
-    filters = {"X": ["Videos\\Movies", "Videos\\TV"], "F": ["Videos\\Temporary", "Videos\\Plex"]}
+    scan_runs = {
+        "X": {
+            "Include": ["Videos\\Movies", "Videos\\TV"],
+            "Exclude": ["Windows"],
+            "Tag": "all",
+            "Output": "C:/WhatsGone",
+            "Backup": "D:/WhatsGone"
+        },
+        "F": {
+            "Include": ["Videos\\Temporary", "Videos\\Plex"],
+            "Exclude": ["Windows"],
+            "Tag": "all",
+            "Output": "C:/WhatsGone",
+            "Backup": "D:/WhatsGone"
+        }
+    }
     output_folder = "C:/WhatsGone"
 
-    for drive, directories in filters.items():
+    for drive, scan_parameters in scan_runs.items():
+        if "Include" in scan_parameters:
+            include_filter = scan_parameters["Include"]
+        else:
+            include_filter = []
+        
+        if "Exclude" in scan_parameters:
+            exclude_filter = scan_parameters["Exclude"] 
+        else:
+            exclude_filter = []
+
+        if "Tag" in scan_parameters:
+            tag = scan_parameters["Tag"] 
+        else:
+            tag = "default"
+
+        scan_drive = drive + ":"
+        output_file_path = rf"{scan_parameters['Output']}/{drive}_{scan_parameters['Tag']}.txt"
+        files_to_scan = "|".join([f"\"{directory}\"" for directory in include_filter])
+        files_to_skip = "|".join([f"\"{directory}\"" for directory in exclude_filter])
 
         # Skip to the next item if the drive does not exist
         if not os.path.exists(drive + ":\\"):
             print(f"MISSING: Drive {drive}:\\ no longer exists. It's gone.")
-            missing_file_path = os.path.join(output_folder, f"{drive}_missing.txt")
+            missing_file_path = os.path.join(scan_parameters["Output"], f"{drive}_missingdrive.txt")
             with open(missing_file_path, 'a', encoding='utf-8') as missing_file:
                 missing_file.write(f"Drive {drive}:\\ is missing as of {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.\n")
             print(f"INFO: Missing drive note written to {missing_file_path}")
             continue
 
-        scan_drive = drive + ":"
-        directory_to_scan = "|".join([f"\"{directory}\"" for directory in directories])
-        output_file_path = rf"{output_folder}/{drive}_all.txt"
-
-        get_files_with_wiztree(wiztree_executable, scan_drive, directory_to_scan, output_file_path)
+        get_files_with_wiztree(wiztree_executable, scan_drive, tag, files_to_scan, files_to_skip, output_file_path)
         process_output_file(output_file_path)
+
+        if "Backup" in scan_parameters:
+            backup_file_path = rf"{scan_parameters['Backup']}/{drive}_{scan_parameters['Tag']}.txt"
+            ensure_folder_exists(backup_file_path)
+
+            # Copy the output file to the backup location, overwriting if it exists
+            try:
+                shutil.copy(output_file_path, backup_file_path)
+                print(f"COMPLETED: {output_file_path} has been backed up to {backup_file_path}.")
+            except Exception as e:
+                print(f"ERROR: Failed to back up {output_file_path} to {backup_file_path}. Reason: {e}")
+            
+        
+        
